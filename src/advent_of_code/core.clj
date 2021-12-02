9e8d82dc9dae2ebfea41963aec2ce5ee3289a945
Andrew Whitaker <agwhitaker93@gmail.com>
2021-12-02 16:24:15 +0000

    day 1

---
+++ src/advent_of_code/core.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code.core
+  (:require [advent-of-code.day-1 :as day-1])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (day-1/part-1))
+  (println (day-1/part-2)))
